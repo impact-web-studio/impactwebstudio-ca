97e6ef0929cdf4e0f297829891f95f41706f363f
kenny-nguyen <developedbykenny@gmail.com>
2025-03-13 14:35:28 +0700

    add separate mobile menu component

---
+++ src/_data/navigation.js
@@ -0,0 +1,9 @@
+// [Data] - Navigation Links
+export default [
+	{ text: 'Home', url: '/', type: 'default', active: true },
+	{ text: 'About', url: '/about', type: 'default', active: false },
+	{ text: 'Services', url: '/services', type: 'default', active: false },
+	{ text: 'Portfolio', url: '/portfolio', type: 'default', active: false },
+	{ text: 'Pricing', url: '/pricing', type: 'default', active: false },
+	{ text: 'Contact', url: '/pricing', type: 'cta', active: false },
+];
