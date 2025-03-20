5de281807293fae3e42cd3d47828e96160b80371
kenny-nguyen <developedbykenny@gmail.com>
2025-03-20 11:47:12 +0700

    add footer styles on mobile and tablet

---
+++ src/_data/client.js
@@ -0,0 +1,30 @@
+export const client = [
+	{
+		client: 'Client 1',
+	},
+];
+
+export const FooterInfo = [
+	{
+		field: 'Email',
+		value: 'info@impactwebstudio.ca',
+		link: 'mailto:info@impactwebstudio.ca',
+		icon: 'mail',
+	},
+	{
+		field: 'WhatsApp',
+		value: '+1 (647) 767-8840',
+		link: 'https://wa.me/16477678840',
+		icon: 'whatsapp',
+	},
+	{
+		field: 'Hours',
+		value: '24/7',
+		icon: 'clock',
+	},
+	{
+		field: 'Location',
+		value: 'Canada',
+		icon: 'location',
+	},
+];
