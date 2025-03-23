a96873239f76b6aee1a7cc98d43ccf181522f94a
kenny-nguyen <developedbykenny@gmail.com>
2025-03-23 15:12:23 +0700

    add portfolio card layout

---
+++ src/_data/portfolio.js
@@ -0,0 +1,23 @@
+export default [
+	{
+		image: 'blkowned.ca.png',
+		title: 'blk owned',
+		description:
+			'BLK Owned is a non-profit startup in the Hamilton Area that empowers and elevates black owned businesses across the GTA.',
+		url: 'https://blkowned.ca/',
+	},
+	{
+		image: 'hendersonhomeimprovements.ca.png',
+		title: 'henderson home improvements',
+		description:
+			'Henderson Home Improvements is a construction company servicing the GTA.',
+		url: 'https://hendersonhomeimprovements.ca/',
+	},
+	{
+		image: 'hendersonhomeimprovements.ca.png',
+		title: 'henderson home improvements',
+		description:
+			'Henderson Home Improvements is a construction company servicing the GTA.',
+		url: 'https://hendersonhomeimprovements.ca/',
+	},
+];
