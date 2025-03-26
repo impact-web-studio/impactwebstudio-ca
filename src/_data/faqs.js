32207ac2c6a0bad5d6201eba67b933550f96b386
kenny-nguyen <developedbykenny@gmail.com>
2025-03-26 13:55:22 +0700

    add styles for faqs cards

--- src/_data/faqs.js
+++ src/_data/faqs.js
@@ -1,58 +1,58 @@
 export const GeneralFAQs = [
 	{
-		question: 'what is your refund policy?',
+		question: 'What is your refund policy?',
 		answer:
 			"We offer a 30-day money-back guarantee. If you're not happy with your purchase, we'll refund your money, no questions asked.",
 	},
 	{
-		question: 'what is your cancelation policy?',
+		question: 'What is your cancelation policy?',
 		answer:
 			'All plans are able to be canceled. However, for the monthly package, a minimum contract length is 12 months. Any cancellation will incur a fee of the remaining contract length.',
 	},
 	{
-		question: 'how does the risk-free guarantee work?',
+		question: 'How does the risk-free guarantee work?',
 		answer: 'tba',
 	},
 	{
-		question: 'how does the risk-free guarantee work?',
+		question: 'How does the risk-free guarantee work?',
 		answer: 'tba',
 	},
 ];
 
 export const DesignFAQs = [
 	{
-		question: 'what if i dont like the design?',
+		question: 'What if I dont like the design?',
 		answer: 'full refund',
 	},
 	{
-		question: 'how long does it take to design a website?',
+		question: 'How long does it take to design a website?',
 		answer: 'tba',
 	},
 	{
-		question: 'what if i dont like the design?',
+		question: 'What if I dont like the design?',
 		answer: 'full refund',
 	},
 	{
-		question: 'what if i dont like the design?',
+		question: 'What if I dont like the design?',
 		answer: 'full refund',
 	},
 ];
 
 export const DevelopmentFAQs = [
 	{
-		question: 'what if i dont like the design?',
+		question: 'What if I dont like the design?',
 		answer: 'full refund',
 	},
 	{
-		question: 'how long does it take to design a website?',
+		question: 'How long does it take to design a website?',
 		answer: 'tba',
 	},
 	{
-		question: 'what if i dont like the design?',
+		question: 'What if I dont like the design?',
 		answer: 'full refund',
 	},
 	{
-		question: 'what if i dont like the design?',
+		question: 'What if I dont like the design?',
 		answer: 'full refund',
 	},
 ];
