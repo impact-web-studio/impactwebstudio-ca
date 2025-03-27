25078bf7d1f016121b3207d252b933422ad1b040
kenny-nguyen <developedbykenny@gmail.com>
2025-03-27 12:59:06 +0700

    add highlight card component

---
+++ src/_data/keyConsiderations.js
@@ -0,0 +1,5 @@
+export default [
+	'How much time am I spending each week manually scheduling appointments, sending reminders, and handling payments—and what could I do with that time instead?',
+	'If my competitors are offering online booking and payments, how does my business compare in terms of convenience and customer experience?',
+	"How much more revenue could I generate if I could book appointments 24/7, even when I'm not available to answer the phone?",
+];
